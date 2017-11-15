980de6b881d6b9e757827d18ce0e0f5d93f3a3b8
Rajaniemi Jaakko <jaakko.rajaniemi@digia.com>
2017-11-15 17:34:20 +0200

    editing and correcting, now it should work

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.get('/building-info', function(req,res) {
 	var address = req.query.address; // $_GET["address"]
 	var openstreetmap_api = 'http://nominatim.openstreetmap.org/search?street={+path}&format=json&polygon=1&addressdetails=1'
-	var vantaa_api = 'http://geoserver.hel.fi/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&typeName=osm:van_rakennukset_dist&outputFormat=json&count=2&SRSNAME=EPSG:4326&BBOX={+bbox},EPSG:4326'
+	var vantaa_api = 'http://geoserver.hel.fi/geoserver/wfs?service=wfs&version=2.0.0&request=GetFeature&typeName=osm:van_rakennukset_dist&outputFormat=json&count=100&SRSNAME=EPSG:4326&BBOX={+bbox},EPSG:4326'
 	var r; 
 	const openstreetmap_api_uri = openstreetmap_api.replace('{+path}', address);
 	console.log('uri', openstreetmap_api_uri)
@@ -26,29 +26,62 @@ app.get('/building-info', function(req,res) {
 		.then(function(body) {
 			r = body.parsed[0];
 			console.log('parsed', JSON.stringify(r));
-			console.log('bbox', r.boundingbox);
-			const d = 0.0
-			const vantaa_api_uri = vantaa_api.replace('{+bbox}', (parseFloat(r.boundingbox[2])-d )+','+(parseFloat(r.boundingbox[0])-d)+','+(parseFloat(r.boundingbox[3])+d)+','+(parseFloat(r.boundingbox[1])+d));
+			//console.log('bbox', r.boundingbox);
+			const d = 0.005
+//			const vantaa_api_uri = vantaa_api.replace('{+bbox}', (parseFloat(r.boundingbox[2])-d )+','+(parseFloat(r.boundingbox[0])-d)+','+(parseFloat(r.boundingbox[3])+d)+','+(parseFloat(r.boundingbox[1])+d));
+			const vantaa_api_uri = vantaa_api.replace('{+bbox}', (parseFloat(r.lon)-d )+','+(parseFloat(r.lat)-d/2)+','+(parseFloat(r.lon)+d)+','+(parseFloat(r.lat)+d/2))
 			console.log('vantaa_uri', openstreetmap_api_uri)
 			loadResource(vantaa_api_uri)
 				.then(function(body) {
 					r = body.parsed;
 					console.log('parsed', JSON.stringify(r));
-					res.status(200).send(JSON.stringify(r));
+					var i = 0;
+					var response = {
+						id:'',
+						address: '',
+						building_type: '',
+						built_date: '',
+						building_material: '',
+						exterior_material: '',
+						apartments: '',
+						floors: '',
+						area_sqm:''
+					}
+					//r.features.forEach(function(feature) {
+					for (var j = 0; j < r.features.length; j++) {
+						var feature = r.features[j];
+						if (feature.properties) {						
+							console.log("Webhook received unknown event " + i + ": ", feature.properties.katuosoite_suomeksi);
+							i++;
+							if (feature.properties.katuosoite_suomeksi == address){
+								response.id = feature.properties.kiinteistotunnus
+								response.address = feature.properties.katuosoite_suomeksi
+								response.building_type = feature.properties.käyttötarkoitus
+								response.built_date = feature.properties.valmistumispvm
+								response.building_material = feature.properties.rakennusmateriaali
+								response.exterior_material = feature.properties.julkisivumateriaali
+								response.apartments = feature.properties.asuntoja
+								response.floors = feature.properties.kerrostenlkm
+								response.area_sqm = feature.properties.rakennusala
+								break;
+							}
+						}
+					}
+					res.status(200).send(response);
 				}
 			)
 			}).catch(function(error) {
-			if (error.statusCode === 404) {
-				console.log(`Needs configuration (contact Digia; reference: ${job.arguments.name})`);
-			return BPromise.reject({
-				message: `Needs configuration (contact Digia; reference: ${job.arguments.name})`,
-				statusCode: 202});
-			}
-			console.log(`Configuration loading errored (${error.statusCode}) with: ${error.message}`);
-			return BPromise.reject({
-				message: error.message,
-				statusCode: error.statusCode});
-	});
+				if (error.statusCode === 404) {
+					console.log(`Error 404: ${error.message}`);
+					return BPromise.reject({
+						message: `Error 404: ${error.message}`,
+						statusCode: 202});
+					}
+				console.log(`Error: ${error.message}`);
+				return BPromise.reject({
+					message: error.message,
+					statusCode: error.statusCode});
+		});
 
 //});
 });
@@ -69,7 +102,7 @@ function loadResource(source, headers = {'Accept': 'application/json'}) {
           statusCode: response.statusCode});
       }
       result.unparsed = body;
-	  console.log('body', JSON.stringify(body))
+	  //console.log('body', JSON.stringify(body))
         try {
           result.parsed = JSON.parse(result.unparsed);
           result.type = 'application/json';
